a53e52dba4a4244c7e2d3c23d1fa054085332ac2
Sudhakar Sonkar <su.sonkar@gemsol.in>
2022-08-17 19:56:25 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'my-app';
+  name:any;
+  output: any;
+
+  ReadMore:boolean = true;  
+  visible:boolean = false;
+  
 }
